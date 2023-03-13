049889994a7e60ce1d761ca10ac75c16045eb22b
Trymne <tsnps3@gmail.com>
2023-03-13 11:17:38 +0100

    style update

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-logo"/>
           <p>
             Omg, I made a website!
           </p>
